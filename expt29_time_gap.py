7889f306696da1a59972d91b056c1d166f5be45e
WonHyung Shin <wonhyung64@icloud.com>
2026-03-29 18:08:30 +0900

    feat - expt

--- expt29_time_gap.py
+++ expt29_time_gap.py
@@ -574,7 +574,7 @@ args = parse_args()
 expt_num = f'{datetime.now().strftime("%y%m%d_%H%M%S_%f")}'
 set_seed(args.seed)
 args.device = set_device()
-args.expt_name = f"expt28_gru_{expt_num}"
+args.expt_name = f"expt29_time_gap_{expt_num}"
 args.save_path = f"{args.weights_path}/{args.dataset}"
 os.makedirs(args.save_path, exist_ok=True) 
 

29400bdbb7e5b55508116bc11fe8eb4ffbc77136
wonhyung64 <wonhyung64@icloud.com>
2026-05-06 16:07:35 +0900

    feat - expt

--- baseline/module/debias.py
+++ baseline/module/debias.py
@@ -36,8 +36,8 @@ def build_debias_model(model_class):
             return self.softplus(self.log_beta) + 1e-6
 
         def prior_parameters_from_embeddings(self):
-            # z = nn.functional.normalize(self.p_item_embedding.weight, dim=-1)
-            z = nn.functional.normalize(self.item_embedding.weight, dim=-1)
+            z = nn.functional.normalize(self.p_item_embedding.weight, dim=-1)
+            # z = nn.functional.normalize(self.item_embedding.weight, dim=-1)
             # z = self.p_item_embedding.weight
             # z = self.item_embedding.weight
             mu = self.softplus(self.base_net(z)).squeeze(-1) + 1e-8
@@ -46,8 +46,8 @@ def build_debias_model(model_class):
             return mu, alpha, beta
 
         def prior(self, batch_items, pos_time, batch_time_all):
-            # item_vec = nn.functional.normalize(self.p_item_embedding(batch_items), dim=-1)
-            item_vec = nn.functional.normalize(self.item_embedding(batch_items), dim=-1)
+            item_vec = nn.functional.normalize(self.p_item_embedding(batch_items), dim=-1)
+            # item_vec = nn.functional.normalize(self.item_embedding(batch_items), dim=-1)
             # item_vec = self.p_item_embedding(batch_items)
             # item_vec = self.item_embedding(batch_items)
             beta = self.current_beta()

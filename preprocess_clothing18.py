808d5523a78ecdbcdb8eac3cb433892f2ca5b95d
wonhyung64 <wonhyung64@icloud.com>
2026-05-06 13:26:09 +0900

    feat - expt

---
+++ preprocess_clothing18.py
@@ -0,0 +1,161 @@
+#%%
+import numpy as np
+import pandas as pd
+
+
+#%%
+file_dir = "./data"
+# file = f"{file_dir}/clothing/All_Beauty.jsonl"
+# raw_df = pd.read_json(file, lines=True)
+
+file = "/home/optim1/Desktop/won/ldr_rec/data/clothing18/Clothing_Shoes_and_Jewelry_5.json"
+raw_df = pd.read_json(file, lines=True)
+df = raw_df[["reviewerID", "asin", "overall", "unixReviewTime"]].dropna().reset_index(drop=True)
+
+
+
+#%%o
+df.columns = ["user_id", "item_id", "rating", "timestamp"]
+df["rating"].value_counts()
+df = df.loc[df["rating"] >= 4, ["user_id", "item_id", "timestamp"]].reset_index(drop=True)
+
+df["user_id"], user_map = pd.factorize(df["user_id"])
+df["item_id"], item_map = pd.factorize(df["item_id"])
+
+train_time_upper = df["timestamp"].quantile(0.8)
+valid_time_upper = df["timestamp"].quantile(0.9)
+train_idx = df[df["timestamp"] <= train_time_upper].index
+valid_idx = df[(df["timestamp"] > train_time_upper) & (df["timestamp"] <= valid_time_upper)].index
+test_idx = df[df["timestamp"] > valid_time_upper].index
+
+#%%
+from tqdm import tqdm
+
+_train_dict, _valid_dict, _test_dict, _time_dict = {}, {}, {}, {}
+df = df.sort_values(["user_id", "timestamp"])
+
+for u in tqdm(df["user_id"].unique().tolist()):
+    u_df = df[df["user_id"] == u][["item_id", "timestamp"]]
+    train_time_bool = u_df["timestamp"] <= train_time_upper
+    valid_time_bool = (u_df["timestamp"] > train_time_upper) & (u_df["timestamp"] <= valid_time_upper)
+    test_time_bool = u_df["timestamp"] > valid_time_upper
+
+    _train_dict[u] = u_df[train_time_bool]["item_id"].tolist()
+    _valid_dict[u] = u_df[valid_time_bool]["item_id"].tolist()
+    _test_dict[u] = u_df[test_time_bool]["item_id"].tolist()
+    _time_dict[u] = dict(zip(u_df["item_id"], u_df["timestamp"]))
+
+
+
+#%%
+
+train_df = pd.read_csv(train_dir)
+valid_df = pd.read_csv(valid_dir)
+test_df = pd.read_csv(test_dir)
+
+#%%
+train_df = train_df[["user_id", "parent_asin", "rating", "timestamp"]]
+valid_df = valid_df[["user_id", "parent_asin", "rating", "timestamp"]]
+test_df = test_df[["user_id", "parent_asin", "rating", "timestamp"]]
+
+train_df.columns = ["user_id", "item_id", "rating", "timestamp"]
+valid_df.columns = ["user_id", "item_id", "rating", "timestamp"]
+test_df.columns = ["user_id", "item_id", "rating", "timestamp"]
+
+train_df["split"] = "train"
+valid_df["split"] = "valid"
+test_df["split"] = "test"
+
+df = pd.concat([train_df, valid_df, test_df])
+
+
+#%%
+df = df.loc[df["rating"] >= 4, ["user_id", "item_id", "timestamp", "split"]].reset_index(drop=True)
+df = df[df["item_id"].isin(df["item_id"].value_counts()[(df["item_id"].value_counts() >= 5)].index.tolist())].reset_index(drop=True)
+
+
+#%%
+df["user_id"], user_map = pd.factorize(df["user_id"])
+df["item_id"], item_map = pd.factorize(df["item_id"])
+
+
+# %%
+from tqdm import tqdm
+
+_train_dict, _valid_dict, _test_dict, _time_dict = {}, {}, {}, {}
+
+df = df.sort_values(["user_id", "timestamp"])
+
+for u, u_df in tqdm(df.groupby("user_id", sort=False), total=df["user_id"].nunique()):
+    _train_dict[u] = u_df.loc[u_df["split"].eq("train"), "item_id"].tolist()
+    _valid_dict[u] = u_df.loc[u_df["split"].eq("valid"), "item_id"].tolist()
+    _test_dict[u]  = u_df.loc[u_df["split"].eq("test"),  "item_id"].tolist()
+
+    _time_dict[u] = dict(zip(u_df["item_id"].to_numpy(), u_df["timestamp"].to_numpy()))
+
+#%%
+train_dict, valid_dict, test_dict, time_dict = {}, {}, {}, {}
+train_user_list = list(_train_dict.keys())
+train_user_list.sort()
+
+user_idx = 0
+exclude_num = 0
+for u in train_user_list:
+    v = _train_dict[u]
+    if len(v) < 5:
+        exclude_num += 1
+    else:
+        train_dict[user_idx] = _train_dict[u]
+        valid_dict[user_idx] = _valid_dict[u]
+        test_dict[user_idx] = _test_dict[u]
+        time_dict[user_idx] = _time_dict[u]
+        user_idx += 1
+
+
+len(train_dict)
+#%%
+np.save(f"{file_dir}/beauty/training_dict.npy", train_dict, allow_pickle=True)
+np.save(f"{file_dir}/beauty/testing_dict.npy", test_dict, allow_pickle=True)
+np.save(f"{file_dir}/beauty/validation_dict.npy", valid_dict, allow_pickle=True)
+np.save(f"{file_dir}/beauty/interaction_time_dict.npy", time_dict, allow_pickle=True)
+
+
+# %%
+user_set, item_set = [], []
+train_num, valid_num, test_num = 0, 0, 0
+
+for k,v in train_dict.items():
+    user_set.append(k)
+    item_set.extend(v)
+    train_num += len(v)
+
+for k,v in valid_dict.items():
+    user_set.append(k)
+    item_set.extend(v)
+    valid_num += len(v)
+
+for k,v in test_dict.items():
+    user_set.append(k)
+    item_set.extend(v)
+    test_num += len(v)
+
+print(f"user num: {len(set(user_set))} / item num: {len(set(item_set))}")
+print(f"train num: {train_num} / valid num: {valid_num} / test num: {test_num}")
+# %%
+train_items = []
+for k,v in train_dict.items():
+    train_items.extend(v)
+
+test_items = []
+for k,v in test_dict.items():
+    test_items.extend(v)
+
+# %%
+train = list(set(train_items))
+train.sort()
+
+test = list(set(test_items))
+test.sort()
+
+tmp = [i in train for i in test]
+print(sum(tmp) / len(tmp))

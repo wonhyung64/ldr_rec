5145dbbf3ae282d2f8203d9e098bde2e3f54dd9e
wonhyung64 <wonhyung64@icloud.com>
2026-05-18 13:59:12 +0900

    Feat - expt

---
+++ run_1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+experiments=(
+
+    "./baseline/debiased_seq_rec.py --model-name=sasrec --dataset=micro_video --seed=1 --tau=0.1 --lambda1=0.9 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500"
+    "./baseline/debiased_seq_rec.py --model-name=sasrec --dataset=micro_video --seed=2 --tau=0.1 --lambda1=0.9 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500"
+    "./baseline/debiased_seq_rec.py --model-name=sasrec --dataset=micro_video --seed=3 --tau=0.1 --lambda1=0.9 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500"
+    "./baseline/debiased_seq_rec.py --model-name=sasrec --dataset=micro_video --seed=4 --tau=0.1 --lambda1=0.9 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500"
+
+)
+
+ENV=/home1/wonhyung64/anaconda3/envs/openmmlab/bin/python3
+DATADIR=/home1/wonhyung64/Github/ldr_rec/data
+DEVICE0=cuda:0
+DEVICE1=cuda:1
+DEVICE2=cuda:2
+DEVICE3=cuda:3
+
+$ENV ${experiments[0]} --data_path=$DATADIR --device=$DEVICE0 &
+$ENV ${experiments[1]} --data_path=$DATADIR --device=$DEVICE1 &
+$ENV ${experiments[2]} --data_path=$DATADIR --device=$DEVICE2 & 
+$ENV ${experiments[3]} --data_path=$DATADIR --device=$DEVICE3 & 

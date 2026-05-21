89e8a92e011a0c383107b77ba85296cfd7ac124f
wonhyung64 <wonhyung64@icloud.com>
2026-05-21 13:50:04 +0900

    feat - exptg

--- conti_1.sh
+++ conti_1.sh
@@ -4,9 +4,9 @@
 experiments=(
 
     "./baseline/debiased_seq_rec.py --model-name=sasrec --dataset=kuairand --seed=0 --tau=0.1 --lambda1=0.1 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500"
-    "./baseline/debiased_seq_rec.py --model-name=sasrec --dataset=kuairand --seed=0 --tau=0.1 --lambda1=0.7 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500"
-    "./baseline/debiased_seq_rec.py --model-name=sasrec --dataset=kuairand --seed=0 --tau=0.1 --lambda1=0.9 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500"
-    "./baseline/debiased_seq_rec.py --model-name=bsarec --dataset=kuairand --seed=0 --tau=0.1 --lambda1=0.3 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500 --alpha=0.9 --c=1"
+    # "./baseline/debiased_seq_rec.py --model-name=sasrec --dataset=kuairand --seed=0 --tau=0.1 --lambda1=0.7 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500"
+    # "./baseline/debiased_seq_rec.py --model-name=sasrec --dataset=kuairand --seed=0 --tau=0.1 --lambda1=0.9 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500"
+    # "./baseline/debiased_seq_rec.py --model-name=bsarec --dataset=kuairand --seed=0 --tau=0.1 --lambda1=0.3 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500 --alpha=0.9 --c=1"
 
     # "./baseline/debiased_seq_rec.py --model-name=bsarec --dataset=kuairand --seed=0 --tau=0.1 --lambda1=0.5 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500 --alpha=0.9 --c=1"
     # "./baseline/debiased_seq_rec.py --model-name=bsarec --dataset=kuairand --seed=0 --tau=0.1 --lambda1=0.7 --dr-anchor=user --pair-reset-interval=5 --evaluate-interval=500 --epochs=500 --alpha=0.9 --c=1"
@@ -47,9 +47,9 @@ DEVICE2=cuda:2
 DEVICE3=cuda:3
 
 $ENV ${experiments[0]} --data_path=$DATADIR --device=$DEVICE0 &
-sleep 20
-$ENV ${experiments[1]} --data_path=$DATADIR --device=$DEVICE1 &
-sleep 20
-$ENV ${experiments[2]} --data_path=$DATADIR --device=$DEVICE2 & 
-sleep 20
-$ENV ${experiments[3]} --data_path=$DATADIR --device=$DEVICE3 & 
+# sleep 20
+# $ENV ${experiments[1]} --data_path=$DATADIR --device=$DEVICE1 &
+# sleep 20
+# $ENV ${experiments[2]} --data_path=$DATADIR --device=$DEVICE2 & 
+# sleep 20
+# $ENV ${experiments[3]} --data_path=$DATADIR --device=$DEVICE3 & 

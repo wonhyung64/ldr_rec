5a89cb71f9a12004f4c9d66444de6aab8806dfea
wonhyung64 <wonhyung64@icloud.com>
2026-05-22 09:53:20 +0900

    feat - expt

--- conti_3.sh
+++ conti_3.sh
@@ -48,8 +48,8 @@ DEVICE3=cuda:3
 
 # $ENV ${experiments[0]} --data_path=$DATADIR --device=$DEVICE0 &
 # sleep 20
-$ENV ${experiments[1]} --data_path=$DATADIR --device=$DEVICE1 &
+# $ENV ${experiments[1]} --data_path=$DATADIR --device=$DEVICE1 &
 # sleep 20
 # $ENV ${experiments[2]} --data_path=$DATADIR --device=$DEVICE2 & 
 # sleep 20
-# $ENV ${experiments[3]} --data_path=$DATADIR --device=$DEVICE3 & 
+$ENV ${experiments[3]} --data_path=$DATADIR --device=$DEVICE3 & 

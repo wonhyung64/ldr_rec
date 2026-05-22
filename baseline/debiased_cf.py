6562c539aec3692fba28e9f6575c05fe347142cf
WonHyung Shin <wonhyung64@icloud.com>
2026-05-23 06:00:16 +0900

    feat - expt

--- baseline/debiased_cf.py
+++ baseline/debiased_cf.py
@@ -213,7 +213,7 @@ while epoch < args.epochs:
             "model_state_dict": model.state_dict(),
             "optimizer_state_dict": optimizer.state_dict(),
             "loss": epoch_user_loss,
-        }, f"_{args.save_path}/{args.model_name}_lambda{args.lambda1}_e{epoch}_seed{args.seed}.pt")
+        }, f"{args.save_path}/_{args.model_name}_lambda{args.lambda1}_e{epoch}_seed{args.seed}.pt")
 
     if epoch % args.pair_reset_interval == 0:
         if args.dr_anchor != "item":

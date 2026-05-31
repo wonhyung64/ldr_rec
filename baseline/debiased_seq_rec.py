e1e9bab10a89b40585aac9d1e4190086dd237765
wonhyung64 <wonhyung64@icloud.com>
2026-05-31 18:41:39 +0900

    feat - expt

--- baseline/debiased_seq_rec.py
+++ baseline/debiased_seq_rec.py
@@ -126,7 +126,7 @@ if args.dr_anchor != "user":
 epoch = 0
 
 save_dir = Path(args.save_path)
-pattern = f"_{args.model_name}_lambda{args.lambda1}_e???_seed{args.seed}.pt"
+pattern = f"_{args.model_name}_lambda{args.lambda1}_e???_seed{args.seed}_shared{args.shared}.pt"
 matched_files = sorted(save_dir.glob(pattern))
 if len(matched_files) > 0:
     recent_file = max(matched_files, key=get_epoch)
@@ -223,7 +223,7 @@ while epoch < args.epochs:
             "model_state_dict": model.state_dict(),
             "optimizer_state_dict": optimizer.state_dict(),
             "loss": epoch_user_loss,
-        }, f"{args.save_path}/_{args.model_name}_lambda{args.lambda1}_e{epoch}_seed{args.seed}.pt")
+        }, f"{args.save_path}/_{args.model_name}_lambda{args.lambda1}_e{epoch}_seed{args.seed}_shared{args.shared}.pt")
 
 
     if epoch % args.pair_reset_interval == 0:

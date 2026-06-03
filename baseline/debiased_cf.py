9c30a2c0057da42655617cc2b215dea84d2457d7
wonhyung64 <wonhyung64@icloud.com>
2026-06-03 19:03:08 +0900

    feat - expt

--- baseline/debiased_cf.py
+++ baseline/debiased_cf.py
@@ -16,7 +16,7 @@ from module.utils import parse_args, set_seed, set_device
 from module.procedure import computeTopNAccuracy
 from module.dataset import UserItemTime
 from module.model import score_pair, score_all, MODEL_REGISTRY
-from module.debias import build_debias_model, build_unshared_debias_model, build_linear_debias_model
+from module.debias import build_debias_model, build_unshared_debias_model, build_linear_debias_model, build_unshared_linear_debias_model
 from module.sampler import make_prior_snapshot, sample_epoch_negatives
 
 
@@ -75,6 +75,8 @@ elif args.ablation == "linear":
     debiased_class = build_linear_debias_model(model_class)
 elif args.ablation == "none": 
     debiased_class = build_debias_model(model_class)
+elif args.ablation == "both":
+    debiased_class = build_unshared_linear_debias_model(model_class)
 
 model = debiased_class(
     num_users=dataset.n_user,

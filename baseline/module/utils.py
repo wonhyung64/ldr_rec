757f2129458235d5dd58cf5bdc698259c0e75c37
WonHyung Shin <wonhyung64@icloud.com>
2026-06-24 15:10:07 +0900

    feat - expt

--- baseline/module/utils.py
+++ baseline/module/utils.py
@@ -45,6 +45,7 @@ def parse_args():
     parser.add_argument('--n-proto', type=int, default=128)
     parser.add_argument('--melt-alpha', type=float, default=0.5)
     parser.add_argument('--k-pop', type=int, default=1)
+    parser.add_argument('--ablation2', type=str, default="none")
 
 
 
